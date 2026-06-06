144dd2bb18e1a10c720a209e1d548c5da57ffd96
nadiaperesoficial-hash <nadiaperesoficial@gmail.com>
2026-06-06 15:02:41 -0300

    Update home_screen.dart

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:get/get.dart';
 
-import '../helper/ad_helper.dart';
 import '../helper/global.dart';
 import '../helper/pref.dart';
 import '../model/home_type.dart';
@@ -16,8 +15,7 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  // final _isDarkMode = Pref.isDarkMode.obs;
-  final _isDarkMode = Get.isDarkMode.obs; //bug fix
+  final _isDarkMode = Get.isDarkMode.obs;
 
   @override
   void initState() {
@@ -28,25 +26,17 @@ class _HomeScreenState extends State<HomeScreen> {
 
   @override
   Widget build(BuildContext context) {
-    //initializing device size
     mq = MediaQuery.sizeOf(context);
 
-    //sample api call
-    // APIs.getAnswer('hii');
-
     return Scaffold(
-      //app bar
       appBar: AppBar(
         title: const Text(appName),
-
-        //
         actions: [
           IconButton(
               padding: const EdgeInsets.only(right: 10),
               onPressed: () {
                 Get.changeThemeMode(
                     _isDarkMode.value ? ThemeMode.light : ThemeMode.dark);
-
                 _isDarkMode.value = !_isDarkMode.value;
                 Pref.isDarkMode = _isDarkMode.value;
               },
@@ -57,11 +47,6 @@ class _HomeScreenState extends State<HomeScreen> {
                   size: 26)))
         ],
       ),
-
-      //ad
-      bottomNavigationBar: AdHelper.nativeBannerAd(),
-
-      //body
       body: ListView(
         padding: EdgeInsets.symmetric(
             horizontal: mq.width * .04, vertical: mq.height * .015),
