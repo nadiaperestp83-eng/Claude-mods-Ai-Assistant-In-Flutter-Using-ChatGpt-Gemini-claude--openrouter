8534c08197f969421b1395e24fe9c219f85be103
nadiaperestp83-eng <nadiaperestp83@gmail.com>
2026-06-22 17:32:03 -0300

    Update message.dart

--- lib/model/message.dart
+++ lib/model/message.dart
@@ -2,11 +2,13 @@ class Message {
   String msg;
   final MessageType msgType;
   final String? aiProvider;
+  final String? videoUrl;
 
   Message({
     required this.msg,
     required this.msgType,
     this.aiProvider,
+    this.videoUrl,
   });
 }
 
